5b479766624767741d31827eee3fa5518aea3c70
yugure <109891005+yugure-orca@users.noreply.github.com>
2023-09-19 01:14:15 +0900

    block_processor

--- src/worker/block_processor.ts
+++ src/worker/block_processor.ts
@@ -4,11 +4,13 @@ import { strToU8, strFromU8, gzipSync, gunzipSync } from "fflate";
 import { Block, Slot, SlotProcessingState } from "../types";
 import invariant from "tiny-invariant";
 
-//import JSONBigInt from "json-bigint";
-//import BigNumber from "bignumber.js";
+import JSONBigInt from "json-bigint";
+import BigNumber from "bignumber.js";
+
+import { WhirlpoolTransactionDecoder } from "@yugure-orca/whirlpool-tx-decoder";
 
 // change BigNumber config to never use exponential notation
-//BigNumber.config({ EXPONENTIAL_AT: 1e9 });
+BigNumber.config({ EXPONENTIAL_AT: 1e9 });
 
 export async function processBlock(database: Connection, solana: AxiosInstance, slot: number) {
   const [{ state, blockHeight }] = await database.query<Slot[]>('SELECT * FROM slots WHERE slot = ?', [slot]);
@@ -22,7 +24,35 @@ export async function processBlock(database: Connection, solana: AxiosInstance, 
   invariant(querySlot === slot, "slot must match");
 
   const jsonString = strFromU8(gunzipSync(gzJsonString));
-  console.log("block", slot, jsonString);
+  const json = JSONBigInt.parse(jsonString);
+
+  // sanity check
+  invariant(json.result, "result must exist");
+  invariant(json.result.blockHeight === blockHeight, "blockHeight must match");
+  invariant(JSONBigInt.stringify(json) === jsonString, "some data seems to be lost during JSON parse/stringify");
+
+  const blockData = json.result;
+
+  console.log("transactions", blockData.transactions.length);
+
+  const WHIRLPOOL_PUBKEY = "whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc";
+
+  for (const tx of blockData.transactions) {
+    const readonlyPubkeys = tx.meta.loadedAddresses.readonly;
+    const writablePubkeys = tx.meta.loadedAddresses.writable;
+    const staticPubkeys = tx.transaction.message.accountKeys;
+
+    const allPubkeys = [...staticPubkeys, ...readonlyPubkeys, ...writablePubkeys];
+    const touchWhirlpool = allPubkeys.includes(WHIRLPOOL_PUBKEY);
+
+    if (!touchWhirlpool) continue;
+
+    console.log("whirlpool touched", tx.meta.err === null ? "ok " : "err", tx.transaction.signatures[0]);
+    if (tx.meta.err !== null) continue;
+
+    const whirlpoolEvents = WhirlpoolTransactionDecoder.decode({ result: tx }, new PublicKey(WHIRLPOOL_PUBKEY));
+    console.log(whirlpoolEvents);
+  }
 
 /*
   await database.beginTransaction();
@@ -36,6 +66,7 @@ export async function processBlock(database: Connection, solana: AxiosInstance, 
 import { DB_CONNECTION_CONFIG, SOLANA_RPC_URL } from "../constants";
 import { createConnection } from "mariadb";
 import axios from "axios";
+import { PublicKey } from "@solana/web3.js";
 
 async function main() {
   const database = await createConnection(DB_CONNECTION_CONFIG);
