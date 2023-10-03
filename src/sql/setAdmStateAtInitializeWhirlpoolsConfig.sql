5ac49520f5aaafcb40d9249728905a2da87395fe
yugure <109891005+yugure-orca@users.noreply.github.com>
2023-10-03 15:58:25 +0900

    add genesis record

---
+++ src/sql/setAdmStateAtInitializeWhirlpoolsConfig.sql
@@ -0,0 +1,5 @@
+-- Whirlpool: InitializeConfig
+-- 2LecshUwdy9xi7meFgHtFJQNSKk4KdTrcpvaB56dP2NQ
+-- https://solscan.io/tx/2SXy7PHYgXdWs4SXHyuaM8ypxsJtL71Bqx5XFEAA3qwouACd2EsWR3qHdqLBtyssMC3tRWnYoz2ngadXZE6Q7D3X
+-- Slot: 128191596, April 04, 2022 14:45:28 +UTC
+INSERT INTO admState (latestBlockSlot, latestBlockHeight) VALUES (128191595, 116160648);
---
+++ src/sql/setAdmStateAtWhirlpoolProgramGenesis.sql
@@ -0,0 +1,5 @@
+-- BPF Upgradable Loader: DeployWithMaxDataLen
+-- whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc
+-- https://solscan.io/tx/4QfHs7WnQfxHYHVSG2L3Bphju9FDKZBvr3Abie57XV2RPDEcbCLnCy8R63pCFcxdQyk7LAsbhT4WPtGj4AzFaWaH
+-- Slot: 124152351, March 09, 2022 09:34:16 +UTC
+INSERT INTO admState (latestBlockSlot, latestBlockHeight) VALUES (124152350, 112290417);
