3a669d99b4e8d7703818c939f6b3607f6c90ad80
yugure <109891005+yugure-orca@users.noreply.github.com>
2023-10-03 18:30:03 +0900

    add processor-2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "dispatcher",
       script: "./build/command/dispatcher.js",
-      args: "--mariadb-database localtest0 -i 5",
+      args: "--mariadb-database whirlpool -i 5",
       env: {
         NODE_ENV: "production",
       }
@@ -11,15 +11,23 @@ module.exports = {
     {
       name: "sequencer",
       script: "./build/command/sequencer.js",
-      args: "--mariadb-database localtest0",
+      args: "--mariadb-database whirlpool --solana-rpc-url http://localhost:8899",
       env: {
         NODE_ENV: "production",
       }
     },
     {
-      name: "processor",
+      name: "processor-1",
       script: "./build/command/processor.js",
-      args: "--mariadb-database localtest0 -c 10",
+      args: "--mariadb-database whirlpool --solana-rpc-url http://localhost:8899 -c 10",
+      env: {
+        NODE_ENV: "production",
+      }
+    },
+    {
+      name: "processor-2",
+      script: "./build/command/processor.js",
+      args: "--mariadb-database whirlpool --solana-rpc-url http://localhost:8899 -c 10",
       env: {
         NODE_ENV: "production",
       }
