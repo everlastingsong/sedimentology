13699c2c31255309b1befeb7ccc33b4135e9c55e
yugure <109891005+yugure-orca@users.noreply.github.com>
2023-10-03 16:42:00 +0900

    add setAdmState Sep 2023

---
+++ src/sql/setAdmStateAt20230901-000000.sql
@@ -0,0 +1,3 @@
+-- Slot: 214824659, August 31, 2023 23:59:59 +UTC
+-- Slot: 214824664, September 01, 2023 00:00:00 +UTC
+INSERT INTO admState (latestBlockSlot, latestBlockHeight) VALUES (214824659, 197179930);
