35a6dde9557a71cd044337a9a617f3691c048733
yugure <109891005+yugure-orca@users.noreply.github.com>
2023-11-17 12:29:23 +0900

    add functions for read replica

--- src/sql/addReadReplicaSpecific.sql
+++ src/sql/addReadReplicaSpecific.sql
@@ -1,3 +1,6 @@
+--
+-- INDEX
+--
 -- admState
 -- admQueuedSlots
 -- admBackfillSlots
@@ -11,7 +14,6 @@ CREATE UNIQUE INDEX signature ON txs (signature);
 CREATE INDEX payer ON txs (payer);
 
 -- pubkeys
-
 -- balances
 
 -- ixsAdminIncreaseLiquidity
@@ -61,7 +63,6 @@ CREATE INDEX keyTickArrayLower ON ixsIncreaseLiquidity (keyTickArrayLower);
 CREATE INDEX keyTickArrayUpper ON ixsIncreaseLiquidity (keyTickArrayUpper);
 
 -- ixsInitializeConfig
-
 -- ixsInitializeFeeTier
 
 -- ixsInitializePool
@@ -133,3 +134,63 @@ CREATE INDEX keyWhirlpoolTwo ON ixsTwoHopSwap (keyWhirlpoolTwo);
 -- ixsUpdateFeesAndRewards
 CREATE INDEX keyWhirlpool ON ixsUpdateFeesAndRewards (keyWhirlpool);
 CREATE INDEX keyPosition ON ixsUpdateFeesAndRewards (keyPosition);
+
+--
+-- FUNCTION
+--
+DELIMITER ;;
+
+CREATE FUNCTION toSignatureBase58(txidBigint bigint) RETURNS varchar(96) CHARSET utf8mb4 COLLATE utf8mb4_bin
+DETERMINISTIC
+BEGIN
+   DECLARE signatureBase58 varchar(96) CHARSET utf8mb4 COLLATE utf8mb4_bin;
+   SELECT signature INTO signatureBase58 FROM txs WHERE txid = txidBigint;
+   RETURN signatureBase58;
+END;;
+
+CREATE FUNCTION fromSignatureBase58(signatureBase58 varchar(96) CHARSET utf8mb4 COLLATE utf8mb4_bin) RETURNS bigint(11)
+DETERMINISTIC
+BEGIN
+   DECLARE txidBigint bigint;
+   SELECT txid INTO txidBigint FROM txs WHERE signature = signatureBase58;
+   RETURN txidBigint;
+END;;
+
+CREATE FUNCTION toSlot(txid bigint) RETURNS bigint(11)
+DETERMINISTIC
+BEGIN
+   RETURN txid >> 24;
+END;;
+
+CREATE FUNCTION fromSlot(slot bigint) RETURNS bigint(11)
+DETERMINISTIC
+BEGIN
+   RETURN slot << 24;
+END;;
+
+-- shorthand
+CREATE FUNCTION toSignature(txid bigint) RETURNS varchar(96) CHARSET utf8mb4 COLLATE utf8mb4_bin
+DETERMINISTIC
+BEGIN
+   RETURN toSignatureBase58(txid);
+END;;
+
+CREATE FUNCTION fromSignature(signature varchar(96) CHARSET utf8mb4 COLLATE utf8mb4_bin) RETURNS bigint(11)
+DETERMINISTIC
+BEGIN
+   RETURN fromSignatureBase58(signature);
+END;;
+
+CREATE FUNCTION toPubkey(pubkeyId int) RETURNS varchar(64) CHARSET utf8mb4 COLLATE utf8mb4_bin
+DETERMINISTIC
+BEGIN
+   RETURN toPubkeyBase58(pubkeyId);
+END;;
+
+CREATE FUNCTION fromPubkey(pubkeyBase58 varchar(64) CHARSET utf8mb4 COLLATE utf8mb4_bin) RETURNS int(11)
+DETERMINISTIC
+BEGIN
+   RETURN fromPubkeyBase58(pubkeyBase58);
+END;;
+
+DELIMITER ;
