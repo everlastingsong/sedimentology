a9d533f09cee992c228935c2944edd0d57d909ee
yugure <109891005+yugure-orca@users.noreply.github.com>
2026-02-25 11:56:10 +0900

    update io.rs (add views)

--- rust_worker/sedimentology-replayer/src/io.rs
+++ rust_worker/sedimentology-replayer/src/io.rs
@@ -216,6 +216,9 @@ pub fn fetch_instructions_in_slot(slot: u64, database: &mut PooledConn) -> Vec<I
           UNION ALL SELECT * FROM vwJsonIxsSetConfigFeatureFlag WHERE txid BETWEEN :s and :e
           UNION ALL SELECT * FROM vwJsonIxsSetTokenBadgeAttribute WHERE txid BETWEEN :s and :e
           UNION ALL SELECT * FROM vwJsonIxsMigrateRepurposeRewardAuthoritySpace WHERE txid BETWEEN :s and :e
+          UNION ALL SELECT * FROM vwJsonIxsSetAdaptiveFeeConstants WHERE txid BETWEEN :s and :e
+          UNION ALL SELECT * FROM vwJsonIxsRepositionLiquidityV2 WHERE txid BETWEEN :s and :e
+          UNION ALL SELECT * FROM vwJsonIxsIncreaseLiquidityByTokenAmountsV2 WHERE txid BETWEEN :s and :e
           ",
           // no ORDER BY clause, sort at the client side
       params! {
